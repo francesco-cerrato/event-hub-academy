64bf08b34b34a43defa0b108cdb31e9d7afe5d24
Francesco Cerrato <Francescocerrato200@gmail.com>
2026-05-18 10:41:55 +0200

    initial commit with maven

---
+++ src/main/java/com/academy/eventhub/EventHubAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.academy.eventhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventHubAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventHubAcademyApplication.class, args);
+	}
+
+}
